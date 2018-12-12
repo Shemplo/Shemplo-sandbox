65a1e1dee4169800d01754cd5ec41ef954801b3c
Shemplo <shemplo@outlook.com>
2018-12-12 08:13:12 +0300

    Python random forest

---
+++ Genome random forest/src/main/java/ru/shemplo/genome/rf/RunCsvComposer.java
@@ -0,0 +1,67 @@
+package ru.shemplo.genome.rf;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import ru.shemplo.snowball.stuctures.Pair;
+import ru.shemplo.snowball.utils.StringManip;
+
+public class RunCsvComposer {
+    
+    private static final Map <String, List <Double>> 
+        values = new HashMap <> ();
+    
+    public static void main (String [] args) throws Exception {
+        readFile ("freqs.csv", ",");
+        readFile ("sklearn-60.txt", " ");
+        readFile ("sklearn-90.txt", " ");
+        readFile ("sklearn-150.txt", " ");
+        readFile ("sklearn-250.txt", " ");
+        
+        Path path = Paths.get ("results", "table.csv");
+        try (
+            PrintWriter pw = new PrintWriter (path.toFile ());
+        ) {
+            pw.println ("id,gene,mcmc,rf60,rf90,rf150,rf250");
+            
+            AtomicInteger counter = new AtomicInteger ();
+            values.entrySet ().stream ()
+            . map     (Pair::fromMapEntry)
+            . forEach (p -> {
+                StringJoiner sj = new StringJoiner (",");
+                sj.add ("" + counter.incrementAndGet ());
+                sj.add (p.F);
+                
+                p.S.stream ()
+                . map     (Objects::toString)
+                . forEach (sj::add);
+                pw.println (sj.toString ());
+            });
+        }
+    }
+    
+    private static void readFile (String fileName, String separator) throws IOException {
+        Path path = Paths.get ("temp", fileName);
+        try (
+            BufferedReader br = Files.newBufferedReader (path);
+        ) {
+            br.readLine (); // skip headers
+            
+            String line = null;
+            while ((line = StringManip.fetchNonEmptyLine (br)) != null) {
+                final String [] vals = line.split (separator);
+                vals [0] = vals [0].replace ("\"", "");
+                
+                values.putIfAbsent (vals [0], new ArrayList <> ());
+                values.get (vals [0]).add (Double.parseDouble (vals [1]));
+            }
+        }
+    }
+    
+}
---
+++ Genome random forest/src/main/python/gse3189.py
@@ -0,0 +1,36 @@
+import numpy  as np
+import pandas as pd
+
+from sklearn.feature_selection import chi2, f_classif, mutual_info_classif
+from sklearn.ensemble  import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.feature_selection import SelectKBest, SelectFromModel, RFECV
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.metrics import accuracy_score, make_scorer
+from sklearn.svm import SVC, LinearSVC
+
+train_input = pd.read_csv ("./input/train.csv", index_col = 0)
+
+feature_names = train_input.columns.values
+train_data    = train_input.values [:, :7504]
+train_target  = train_input.values [:, -1]
+
+estimators = 60 # run parameter
+forest = RandomForestClassifier (n_estimators=estimators,
+                                 random_state=0)
+forest.fit (train_data, train_target)
+
+scorer = make_scorer (accuracy_score)
+scores = scorer (forest, train_data, train_target)
+print "Match score: %f" % scores 
+
+importances = forest.feature_importances_
+std = np.std ([tree.feature_importances_ for tree in forest.estimators_],
+              axis=0)
+indices = np.argsort (importances) [::-1]							  
+
+output = open ("sklearn-%s.txt" % str (estimators), 'w')
+for f in range(train_data.shape[1]):
+	output.write ("%s %f" % (feature_names [indices [f]], importances [indices [f]]))
+	output.write ("\n")
+output.close ()
+
